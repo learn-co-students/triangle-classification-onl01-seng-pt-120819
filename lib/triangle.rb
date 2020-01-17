577f9f51532f50dc4cdc589dccdb059799b3f297
Swishyfishie <pclandreiuk@gmail.com>
2020-01-17 00:14:28 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,11 +11,7 @@ class Triangle
 
   def kind 
     if (self.side1 <= 0 || self.side2 <= 0 || self.side3 <= 0) || (self.side1 + self.side2 <= self.side3 || self.side2 + self.side3 <= self.side1 || self.side1 + self.side3 <= self.side2)   
-      begin
-        raise TriangleError
-      rescue TriangleError => error
-         error.noSizeError
-      end    
+      raise TriangleError
     else 
       if self.side1 == self.side2 && self.side2 == self.side3 
         :equilateral
