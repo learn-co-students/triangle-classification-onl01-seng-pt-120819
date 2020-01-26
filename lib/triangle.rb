bc89583388e567492d0a990770959f029343c4a3
Damely Tineo <damely.tineo07@gmail.com>
2020-01-26 06:48:26 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,27 @@
 class Triangle
-  # write code here
-end
+  attr_accessor :l1, :l2, :l3 
+  
+  def initialize(l1, l2, l3)
+    @l1 = l1
+    @l2 = l2
+    @l3 = l3
+  end
+  
+  def kind 
+    if (@l1<=0) || (@l2<=0) || (@l3<=0) ||
+      (@l1+@l2<=@l3) || (@l1+@l3<=@l2) || (@l2+@l3<=@l1)
+      raise TriangleError
+    end
+    
+    if (@l1 == @l2 && @l2 == @l3)
+      return :equilateral
+    elsif (@l1 == @l2 )||(@l1==@l3)||(@l2==@l3)
+      return :isosceles
+    else 
+      return :scalene
+    end
+  end
+  
+  class TriangleError < StandardError
+  end
+end 
