b523127cf9b71727d545bdd8b9530db7a93e9c19
github username <github email address>
2020-01-29 18:57:06 -0800

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,9 +10,6 @@ class Triangle
       raise TriangleError
     elsif (@side_1 + @side_2 <= @side_3) || (@side_1 + @side_3 <= @side_2) || (@side_2 + @side_3 <= @side_1)
       raise TriangleError
-    elsif (@side_1 + @side_2 + @side_3 == 0)
-      raise TriangleError
-
     else
       if (@side_1 == @side_2) && (@side_2 == @side_3)
         :equilateral
