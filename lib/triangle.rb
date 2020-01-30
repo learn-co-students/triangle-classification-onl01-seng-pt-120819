eb510e69d03f9fcf7a12c580b8e2f62c84794f20
Asha Fernandez <ash.fernan90@gmail.com>
2020-01-30 22:36:36 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
 class Triangle
-  # write code here
+  attr_accessor :eq, :is, :sc
+  
+  def initialize ( eq, is, sc)
+    @eq= eq
+    @is= is
+    @sc= sc
+  end
+  
+  def kind
+  types_of_triangles
+    if eq == is && is == sc
+    :equilateral
+    elsif eq == is || is == sc || eq == sc
+    :isosceles
+    else
+    :scalene
+  end
+end
+
+ def types_of_triangles
+   true_triangles = [(eq + is > sc), (eq + sc > is), (is + sc > eq)]
+   [eq, is, sc].each do |side|
+     true_triangles<< false if side <=0
+     raise TriangleError if true_triangles.include?(false)
+   end
+  end
+  
+ class TriangleError < StandardError 
+   
+   def message
+    "There's an error in your equation!"
+    end
+  end
 end
