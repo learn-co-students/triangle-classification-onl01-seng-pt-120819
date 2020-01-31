1bec546752ebc7506ccce623650cbf595e585edf
dwane richards <dwanerich@gmail.com>
2020-01-31 17:36:46 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,8 +13,7 @@ class Triangle
         if (s1 * s2 * s3) == 0 || (s1 + s2) <= s3 || (s1 + s3) <= s2 || (s2 + s3) <= s1
           begin
           raise TriangleError
-          # rescue TriangleError => error
-          # return error.message
+
           end
 
       elsif s1 == s2 && s1 == s3
@@ -27,11 +26,9 @@ class Triangle
           kind = :scalene
       end
     end
-  end
-
-
     class TriangleError < StandardError
       def message
         puts "this is not a triangle."
       end
     end
+  end
